8908e60318fce946211b46282f8afc4358003028
mariotorres <mario@maniak.com.mx>
2019-06-14 15:55:03 -0500

    Add index.js post page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
+import PostLink from "../components/post-link"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const IndexPage = ({
+                     data: {
+                       allMarkdownRemark: { edges },
+                     },
+                   }) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to our brand new Maniak Blog.</p>
-    <p>We will soon create awesome content.</p>
-    <Link to="/hello-world/">Go to first post</Link>
-  </Layout>
-)
+  return <div>{Posts}</div>
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
